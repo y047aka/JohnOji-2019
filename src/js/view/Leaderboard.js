06ac346fd51231dbe4fe200b2554a79dde9a9a55
y047aka <y047aka@gmail.com>
2019-03-16 07:52:39 +0900

    Update

--- src/js/view/Leaderboard.js
+++ src/js/view/Leaderboard.js
@@ -9,31 +9,31 @@ const TableHead = state =>
     th('Laps'),
     th('Delta'),
     th('Last Lap'),
-    th('mph'),
-    th('Pit Stops'),
-    th('Last Pit')
+    th('kph'),
+    th('Pit Stops')
+//    th('Last Pit')
   ])
 
 const Vehicle = v =>
   tr([
-    td(v.running_position),
-    td(v.vehicle_number),
-    td(v.driver.full_name),
+    td(v.ranking),
+    td(v.number),
+    td(v.driver),
     td({ Chv: 'Chevrolet', Frd: 'Ford', Tyt: 'Toyota' }[v.vehicle_manufacturer]),
-    td(v.laps_completed),
-    td(v.delta > 0 ? v.delta.toFixed(3) : v.delta),
-    td(v.last_lap_time.toFixed(3)),
-    td(v.last_lap_speed.toFixed(1)),
-    td(v.pit_stops.length),
-    td(v.pit_stops.reverse()[0].pit_in_lap_count)
+    td(v.lap),
+    td(v.gap === "" ? "-" : v.gap),
+    td(v.lastlap),
+    td(v.speed),
+    td(v.pitstop)
+//    td(v.pit_stops.reverse()[0].pit_in_lap_count)
   ])
 
 export default state =>
   section([
-		h1(state.run_name),
+		h1(state.params.eventName),
     table({ class: 'leaderboard' }, [
 			TableHead(),
-      state.vehicles.map(vehicle =>
+      state.entries.map(vehicle =>
         Vehicle(vehicle)
       )
     ])
