344bd9d972f4d470c15fc1d1b4a799195a2af5c8
戸塚孝高 <y047aka@totsukayoshitakanoMac-mini.local>
2019-06-14 04:53:26 +0900

    Apply elm-starfighter

---
+++ src/elm/Main.elm
@@ -0,0 +1,230 @@
+module Main exposing (main)
+
+--import Html exposing (Html, text, node, div, header, section, nav, footer, h1, h2, p, a, ul, li, img)
+
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
+import Json.Decode as Decode
+import Json.Decode.Pipeline exposing (hardcoded, optional, required)
+import Time
+
+
+main =
+    Browser.document
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { userState : UserState
+    }
+
+
+type UserState
+    = Init
+    | Loaded Vehicles
+    | Failed Http.Error
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Model Init
+    , fetchJson
+    )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Tick Time.Posix
+    | Recieve (Result Http.Error Vehicles)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Tick _ ->
+            ( model, fetchJson )
+
+        Recieve (Ok vehicles) ->
+            ( { model | userState = Loaded vehicles }, Cmd.none )
+
+        Recieve (Err error) ->
+            ( { model | userState = Failed error }, Cmd.none )
+
+
+fetchJson : Cmd Msg
+fetchJson =
+    Http.get
+        { url = "https://storage.googleapis.com/fiawec-prod/assets/live/WEC/__data.json"
+        , expect = Http.expectJson Recieve userDecoder
+        }
+
+
+
+-- HTTP
+
+
+type alias Vehicle =
+    { runningPosition : Int
+    , vehicleNumber : Int
+    , fullName : String
+
+    -- , vehicleManufacturer : String
+    , lapsCompleted : String
+
+    -- , delta : Float
+    -- , last_lap_time : Float
+    -- , last_lap_speed : Float
+    -- , pitStops : List Stop
+    }
+
+
+type alias Vehicles =
+    List Vehicle
+
+
+type alias Stop =
+    { pit_in_lap_count : Int
+    }
+
+
+userDecoder : Decode.Decoder Vehicles
+userDecoder =
+    Decode.field "entries" (Decode.list vehicle)
+
+
+vehicle : Decode.Decoder Vehicle
+vehicle =
+    Decode.succeed Vehicle
+        |> required "ranking" Decode.int
+        |> required "number" Decode.int
+        |> required "driver" Decode.string
+        -- |> required "vehicle_manufacturer" Decode.string
+        |> required "lap" Decode.string
+
+
+
+-- |> required "delta" Decode.float
+-- |> required "last_lap_time" Decode.float
+-- |> required "last_lap_speed" Decode.float
+-- |> required "pit_stops" (Decode.list stopDecoder)
+
+
+stopDecoder : Decode.Decoder Stop
+stopDecoder =
+    Decode.succeed Stop
+        |> required "pit_in_lap_count" Decode.int
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Time.every 5000 Tick
+
+
+
+-- VIEW
+
+
+view : Model -> Browser.Document Msg
+view model =
+    { title = "Leaderboard - NASCAR"
+    , body =
+        [ siteHeader
+        , node "main"
+            []
+            [ section []
+                [ case model.userState of
+                    Init ->
+                        text ""
+
+                    Loaded vehicles ->
+                        table [ class "leaderboard" ]
+                            [ tr []
+                                [ th [] [ text "Pos" ]
+                                , th [] [ text "#" ]
+                                , th [] [ text "Driver" ]
+                                , th [] [ text "" ]
+                                , th [] [ text "Laps" ]
+                                , th [] [ text "Delta" ]
+                                , th [] [ text "Last Lap" ]
+                                , th [] [ text "mph" ]
+                                , th [] [ text "Pit Stops" ]
+                                ]
+                            , tbody [] (List.map viewRaces vehicles)
+                            ]
+
+                    Failed error ->
+                        div [] [ text (Debug.toString error) ]
+                ]
+            ]
+        , siteFooter
+        ]
+    }
+
+
+viewRaces : Vehicle -> Html Msg
+viewRaces d =
+    -- let
+    --     manufacturer =
+    --         case d.vehicleManufacturer of
+    --             "Chv" ->
+    --                 "Chevrolet"
+    --             "Frd" ->
+    --                 "Ford"
+    --             "Tyt" ->
+    --                 "Toyota"
+    --             _ ->
+    --                 ""
+    -- in
+    tr []
+        [ td [] [ text (String.fromInt d.runningPosition) ]
+        , td [] [ text (String.fromInt d.vehicleNumber) ]
+        , td [] [ text d.fullName ]
+
+        -- , td [] [ text manufacturer ]
+        , td [] [ text d.lapsCompleted ]
+
+        -- , td [] [ text (d.delta |> String.fromFloat) ]
+        -- , td [] [ text (d.last_lap_time |> String.fromFloat) ]
+        -- , td [] [ text (d.last_lap_speed |> String.fromFloat) ]
+        -- , td [] [ ul [] (List.map pitStop (d.pitStops |> List.reverse)) ]
+        ]
+
+
+pitStop : Stop -> Html Msg
+pitStop stop =
+    li [] [ text (stop.pit_in_lap_count |> String.fromInt) ]
+
+
+siteHeader : Html Msg
+siteHeader =
+    Html.header [ class "site-header" ]
+        [ h1 [] [ text "Leaderboard" ]
+        ]
+
+
+siteFooter : Html Msg
+siteFooter =
+    footer [ class "site-footer" ]
+        [ p [ class "copyright" ]
+            [ text "© 2019 "
+            , a [ href "https://y047aka.me", target "_blank" ] [ text "y047aka" ]
+            ]
+        ]
--- src/js/view/SiteHeader.js
+++
@@ -1,6 +0,0 @@
-import { div, header, h1, p, img } from '@hyperapp/html'
-
-export default () =>
-  header({ class: 'site-header' }, [
-    h1('Leaderboard')
-  ])
